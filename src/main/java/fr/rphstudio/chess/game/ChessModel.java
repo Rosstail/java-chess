e0d4e439ceaa54d037e93b9d0a79722ddd3871b7
Anthony Bac <anthonybac@MacBook-Pro-de-admin-4.local>
2020-02-24 13:14:15 +0100

    finished Sigleton

--- src/main/java/fr/rphstudio/chess/game/ChessModel.java
+++ src/main/java/fr/rphstudio/chess/game/ChessModel.java
@@ -4,6 +4,7 @@ import fr.rphstudio.chess.interf.EmptyCellException;
 import fr.rphstudio.chess.interf.IChess;
 import fr.rphstudio.chess.interf.OutOfBoardException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChessModel implements IChess {
@@ -23,12 +24,12 @@ public class ChessModel implements IChess {
 
     @Override
     public ChessType getPieceType(ChessPosition p) throws EmptyCellException, OutOfBoardException {
-        return null;
+        return ChessType.TYP_BISHOP;
     }
 
     @Override
     public ChessColor getPieceColor(ChessPosition p) throws EmptyCellException, OutOfBoardException {
-        return null;
+        return ChessColor.CLR_BLACK;
     }
 
     @Override
@@ -38,7 +39,7 @@ public class ChessModel implements IChess {
 
     @Override
     public List<ChessPosition> getPieceMoves(ChessPosition p) {
-        return null;
+        return new ArrayList<>();
     }
 
     @Override
@@ -48,12 +49,12 @@ public class ChessModel implements IChess {
 
     @Override
     public ChessKingState getKingState(ChessColor color) {
-        return null;
+        return ChessKingState.KING_SAFE;
     }
 
     @Override
     public List<ChessType> getRemovedPieces(ChessColor color) {
-        return null;
+        return new ArrayList<>();
     }
 
     @Override
